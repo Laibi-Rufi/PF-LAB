95e8082b2a78c4278e7bc51345f0f2a06887fb98
Laibi-Rufi <saimroofi@gmail.com>
2024-10-28 20:06:51 +0500

    Table Loop

--- tableloop.cpp
+++ tableloop.cpp
@@ -2,48 +2,14 @@
 using namespace std;
 int main(){
 	
-	int sub,marks;
-	int sum=0;
+	int num;
+	cout<<"Enter a number :"<<endl;
+	cin>>num;
 	
-	cout<<"Enter the total number of subjects :";
-	cin>>sub;
-
-	
-	for(int i=1;i<=sub;i++){
-		 cout<<"Enter the obtained marks";
-	     cin>>marks;
-	     sum=sum+marks;
-	}
-	
-	float average=sum/sub;
-	cout<<"Average"<<average<<endl;
-	
-	if(average>=90 && average>=100){
+	for(int i=1;i<=10;i++){
 		
-		cout<<"Garde A"<<endl;
+		cout<<num<<" x "<<i<<" = "<<num*i<<endl;
 		
 	}
 	
-	else if(average>=80 && average<=90){
-		
-		cout<<"Grade B"<<endl;
-		
-		
-	}
-	else if(average>=70 && average<=80){
-		
-		cout<<"Grade C"<<endl;
-		
-		
-	}
-	else if(average>=60 && average<=70){
-		
-		cout<<"Grade D"<<endl;
-		
-	}
-	else{
-	
-	cout<<"Grade F"<<endl;
-	
-}
 }
